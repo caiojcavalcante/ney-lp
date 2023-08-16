19affe892c552ffa97be38fcf9138ed4b064c770
Caio Cavalcante <danielcaio75@gmail.com>
2023-08-16 17:08:02 -0300

    add gtm tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -9,8 +10,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:ital,wght@0,300;0,400;0,600;1,400&display=swap"
           rel="stylesheet"
         />
+        <Script
+          strategy="afterInteractive"
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5WV3J2JM');`,
+          }}
+        />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5WV3J2JM"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
